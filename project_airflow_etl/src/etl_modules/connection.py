77c9d449f9001447f5d88d3bf1760aa1dabea4ff
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-05-20 19:18:42 -0400

    feat(main): change connection module

--- project_airflow_etl/src/etl_modules/connection.py
+++ project_airflow_etl/src/etl_modules/connection.py
@@ -1,20 +1,30 @@
 import os
 import psycopg2
 from airflow.utils.log.logging_mixin import LoggingMixin
+from dotenv import load_dotenv
+load_dotenv() 
 
 logger = LoggingMixin().log
 
 def get_connection():
     try:
+        host = os.getenv("DB_HOST")
+        port = os.getenv("DB_PORT")
+        dbname = os.getenv("DB_NAME")
+        user = os.getenv("DB_USER")
+        password = os.getenv("DB_PASSWORD")
+        schema = os.getenv("DB_SCHEMA")
+
+        logger.info(f"Trying to connect to DB at {host}:{port} with user {user} and db {dbname}")
+
         conn = psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD")
+            host=host,
+            port=port,
+            dbname=dbname,
+            user=user,
+            password=password
         )
         cur = conn.cursor()
-        schema = os.getenv("DB_SCHEMA")
         if schema:
             cur.execute(f'SET search_path TO {schema};')
         logger.info("✅ Successfully connected to PostgreSQL.")
