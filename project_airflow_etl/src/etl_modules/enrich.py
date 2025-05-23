a135d9e59422119b9cd0f53985b384cece1ac49f
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-05-23 12:54:43 -0400

    feat(dag): edit enrich module

--- project_airflow_etl/src/etl_modules/enrich.py
+++ project_airflow_etl/src/etl_modules/enrich.py
@@ -4,7 +4,7 @@ from airflow.utils.log.logging_mixin import LoggingMixin
 logger = LoggingMixin().log
 
 def enrich_report(df_usd, clp_rate):
-    if df_usd is not None and clp_rate:
+    if isinstance(df_usd, list) and df_usd and "total" in df_usd[0]:
         df = pd.DataFrame(df_usd)
         df["total"] = df["total"].astype(float)
         df["total_clp"] = round(df["total"] * float(clp_rate), 0)
