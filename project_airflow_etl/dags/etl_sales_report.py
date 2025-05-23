c3a7564bbe6597bd736c5e558c509e779272ed46
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-05-23 12:48:46 -0400

    feat(dags): add @task into the dag

--- project_airflow_etl/dags/etl_sales_report.py
+++ project_airflow_etl/dags/etl_sales_report.py
@@ -1,6 +1,8 @@
 from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
 from datetime import datetime
 import pandas as pd
+
 from src.etl_modules.extract import extract_data
 from src.etl_modules.fx import fetch_usd_to_clp
 from src.etl_modules.enrich import enrich_report
@@ -13,36 +15,46 @@ default_args = {
     'retries': 1
 }
 
-@dag(dag_id="sales_etl_dag", default_args=default_args, schedule_interval="@daily", catchup=False)
-def etl_pipeline():
+@dag(
+    dag_id='sales_etl_dag',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    description="Sales ETL: PostgreSQL -> Enrichment -> CSV and Google Sheets"
+)
+def sales_etl_pipeline():
 
     @task()
-    def _extract():
+    def extract():
         return extract_data()
 
     @task()
-    def _fetch_rate():
+    def fetch_fx_rate():
         return fetch_usd_to_clp()
 
     @task()
-    def _enrich(data, rate):
-        df = enrich_report(pd.DataFrame(data), rate)
+    def enrich(data, rate):
+        df = pd.DataFrame(data)
+        df["total"] = df["total"].astype(float)
+        df["total_clp"] = round(df["total"] * float(rate), 0)
         return df.to_dict(orient='records')
 
     @task()
-    def _export_csv(data):
+    def export_csv(data):
         df = pd.DataFrame(data)
         export_results(df)
 
     @task()
-    def _export_gsheet(data):
+    def export_gsheet(data):
         df = pd.DataFrame(data)
         export_to_google_sheets(df)
 
-    data = _extract()
-    rate = _fetch_rate()
-    enriched = _enrich(data, rate)
-    _export_csv(enriched)
-    _export_gsheet(enriched)
+    # Task pipeline
+    raw_data = extract()
+    rate = fetch_fx_rate()
+    enriched_data = enrich(raw_data, rate)
+    export_csv(enriched_data)
+    export_gsheet(enriched_data)
 
-etl_pipeline()
+# DAG instance
+sales_etl_pipeline()
