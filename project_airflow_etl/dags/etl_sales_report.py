68945440d48d50686394e4fddbd702a5dd5ef2fb
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-05-23 12:42:24 -0400

    feat(dags): add @task into the dag

--- project_airflow_etl/dags/etl_sales_report.py
+++ project_airflow_etl/dags/etl_sales_report.py
@@ -1,12 +1,6 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
 from datetime import datetime
 import pandas as pd
-
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from src.etl_modules.extract import extract_data
 from src.etl_modules.fx import fetch_usd_to_clp
 from src.etl_modules.enrich import enrich_report
@@ -19,53 +13,36 @@ default_args = {
     'retries': 1
 }
 
-with DAG(
-    dag_id='sales_etl_dag',
-    default_args=default_args,
-    schedule='@daily',
-    catchup=False
-) as dag:
+@dag(dag_id="sales_etl_dag", default_args=default_args, schedule_interval="@daily", catchup=False)
+def etl_pipeline():
 
-    task_extract = PythonOperator(
-        task_id='extract_data',
-        python_callable=extract_data
-    )
+    @task()
+    def _extract():
+        return extract_data()
 
-    task_fetch = PythonOperator(
-        task_id='fetch_usd_to_clp',
-        python_callable=fetch_usd_to_clp
-    )
+    @task()
+    def _fetch_rate():
+        return fetch_usd_to_clp()
 
-    def _enrich_report(**context):
-        df_usd = context['ti'].xcom_pull(task_ids='extract_data')
-        rate = context['ti'].xcom_pull(task_ids='fetch_usd_to_clp')
-        df = enrich_report(pd.DataFrame(df_usd), rate)
-        context['ti'].xcom_push(key='enriched', value=df.to_dict())
-        return df
+    @task()
+    def _enrich(data, rate):
+        df = enrich_report(pd.DataFrame(data), rate)
+        return df.to_dict(orient='records')
 
-    task_enrich = PythonOperator(
-        task_id='enrich_report',
-        python_callable=_enrich_report
-    )
-
-    def _export(**context):
-        df_dict = context['ti'].xcom_pull(task_ids='enrich_report', key='enriched')
-        df = pd.DataFrame(df_dict)
+    @task()
+    def _export_csv(data):
+        df = pd.DataFrame(data)
         export_results(df)
 
-    task_export_csv = PythonOperator(
-        task_id='export_csv',
-        python_callable=_export
-    )
-
-    def _export_gsheet(**context):
-        df_dict = context['ti'].xcom_pull(task_ids='enrich_report', key='enriched')
-        df = pd.DataFrame(df_dict)
+    @task()
+    def _export_gsheet(data):
+        df = pd.DataFrame(data)
         export_to_google_sheets(df)
 
-    task_export_gsheet = PythonOperator(
-        task_id='export_gsheet',
-        python_callable=_export_gsheet
-    )
+    data = _extract()
+    rate = _fetch_rate()
+    enriched = _enrich(data, rate)
+    _export_csv(enriched)
+    _export_gsheet(enriched)
 
-    task_extract >> task_fetch >> task_enrich >> task_export_csv >> task_export_gsheet
+etl_pipeline()
