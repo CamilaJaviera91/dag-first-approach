550018db3fc7a2a0a8d785cce80d30c1d0cf26c0
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-05-28 17:26:24 -0400

    feat(dag): change data folder

--- project_airflow_etl/src/etl_modules/export.py
+++ project_airflow_etl/src/etl_modules/export.py
@@ -7,5 +7,5 @@ logger = LoggingMixin().log
 def export_results(df_dict):
     df = pd.DataFrame(df_dict)
     os.makedirs("data", exist_ok=True)
-    df.to_csv("../data/report.csv", index=False)
+    df.to_csv("data/report.csv", index=False)
     logger.info("📤 Report exported to data/report.csv")
