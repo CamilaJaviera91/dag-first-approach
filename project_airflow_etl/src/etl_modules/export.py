21b851117bade673adc811aa5fbc5e711188fef6
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-05-29 21:58:37 -0400

    feat(export): edit folder name

--- project_airflow_etl/src/etl_modules/export.py
+++ project_airflow_etl/src/etl_modules/export.py
@@ -4,8 +4,11 @@ from airflow.utils.log.logging_mixin import LoggingMixin
 
 logger = LoggingMixin().log
 
-def export_results(df_dict):
-    df = pd.DataFrame(df_dict)
-    os.makedirs("data", exist_ok=True)
-    df.to_csv("data/report.csv", index=False)
-    logger.info("📤 Report exported to data/report.csv")
+def export_results(df):
+    # Crear carpeta 'data' si no existe
+    output_dir = os.path.join(os.getcwd(), "data") 
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_file = os.path.join(output_dir, "report.csv")
+    df.to_csv(output_file, index=False)
+    logger.info(f"📤 Report exported to {output_file}")
