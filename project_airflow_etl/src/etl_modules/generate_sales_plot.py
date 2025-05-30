a4da1fa3f8297568f5d7271ade46938c530fc611
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-05-29 22:06:09 -0400

    feat(plt): edit plt module so we can work with matplot

--- project_airflow_etl/src/etl_modules/generate_sales_plot.py
+++ project_airflow_etl/src/etl_modules/generate_sales_plot.py
@@ -7,21 +7,21 @@ from pathlib import Path
 def generate_sales_by_year_plot(data: list[dict], output_path: str = 'data/yearly_sales.png'):
     df = pd.DataFrame(data)
 
-    if 'Year' not in df.columns or 'total_clp' not in df.columns:
+    if 'year' not in df.columns or 'total_clp' not in df.columns:
         raise ValueError("Missing required columns in data: 'Year' and 'total_clp'.")
 
-    df['Year'] = df['Year'].astype(int)
+    df['year'] = df['year'].astype(int)
     df['total_clp'] = pd.to_numeric(df['total_clp'])
 
-    sales = df.groupby('Year')['total_clp'].sum().reset_index()
+    sales = df.groupby('year')['total_clp'].sum().reset_index()
 
     plt.figure(figsize=(8, 5))
-    plt.bar(sales['Year'], sales['total_clp'], color='skyblue')
+    plt.bar(sales['year'], sales['total_clp'], color='skyblue')
     plt.title('Total de Ventas Anuales en CLP')
-    plt.xlabel('Año')
+    plt.xlabel('Year')
     plt.ylabel('Total CLP')
     plt.grid(axis='y', linestyle='--', alpha=0.7)
-    plt.xticks(sales['Year'])
+    plt.xticks(sales['year'])
     plt.tight_layout()
 
     Path(output_path).parent.mkdir(parents=True, exist_ok=True)
