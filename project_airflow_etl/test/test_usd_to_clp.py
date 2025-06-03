a1eaacc364b86b35708e8f991fb2ca349f96774b
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-03 15:36:31 -0400

    feat(test): edit 'test_usd_to_clp'

--- project_airflow_etl/test/test_usd_to_clp.py
+++ project_airflow_etl/test/test_usd_to_clp.py
@@ -1,9 +1,16 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from unittest.mock import patch
 from src.etl_modules.usd_to_clp import fetch_usd_to_clp
 
-def test_usd_to_clp_positive_float():
+@patch("src.etl_modules.usd_to_clp.requests.get")
+def test_usd_to_clp_positive_float(mock_get):
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = {"USD": 941.06}
+
     rate = fetch_usd_to_clp()
+    
     assert isinstance(rate, float)
     assert rate > 0
