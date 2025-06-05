bfc4787579a9f25cb08649603481d80e6e829a31
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-05 19:39:28 -0400

    feat(test): new test

--- project_airflow_etl/test/test_extract.py
+++ project_airflow_etl/test/test_extract.py
@@ -1,27 +1,7 @@
 from unittest.mock import patch, MagicMock
-import pandas as pd
-
-from etl_modules.enrich import enrich_report
 from etl_modules.extract import extract_data
 
-@patch("etl_modules.enrich.fetch_usd_to_clp")
-@patch("etl_modules.extract.extract_data")
-def test_enrich_adds_total_clp(mock_extract_data, mock_fetch_usd_to_clp):
-    mock_extract_data.return_value = [
-        {"item": "shoes", "price": 50, "quantity": 2, "total": 100},
-        {"item": "shirt", "price": 25, "quantity": 1, "total": 25}
-    ]
-    mock_fetch_usd_to_clp.return_value = 900.0
-
-    df = enrich_report(mock_extract_data.return_value, mock_fetch_usd_to_clp.return_value)
-
-    assert isinstance(df, pd.DataFrame)
-    assert "total_clp" in df.columns
-    assert df["total_clp"].iloc[0] == 100 * 900.0
-    assert df["total_clp"].iloc[1] == 25 * 900.0
-
-
-@patch("src.etl_modules.extract.connect_to_postgres")
+@patch("etl_modules.extract.connect_to_postgres")
 def test_extract_data_returns_expected_structure(mock_connect):
     fake_rows = [
         (2024, "Santiago-01", 1000),
