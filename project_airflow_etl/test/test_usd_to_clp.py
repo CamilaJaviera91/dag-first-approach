36a04a20c7ff0d348f16845769d5c04cdcb1d3a9
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-07 18:02:28 -0400

    feat(test): test

--- project_airflow_etl/test/test_usd_to_clp.py
+++ project_airflow_etl/test/test_usd_to_clp.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"
 from unittest.mock import patch, MagicMock
 from etl_modules.usd_to_clp import fetch_usd_to_clp
 
-@patch("etl_modules.exchange_rate.requests.get")
+@patch("etl_modules.usd_to_clp.requests.get")
 def test_fetch_usd_to_clp_returns_correct_rate(mock_get):
     fake_response = MagicMock()
     fake_response.json.return_value = {
@@ -22,12 +22,12 @@ def test_fetch_usd_to_clp_returns_correct_rate(mock_get):
     assert isinstance(result, float)
     assert result == 925.37
 
-@patch("etl_modules.exchange_rate.requests.get")
+@patch("etl_modules.usd_to_clp.requests.get")
 def test_fetch_usd_to_clp_missing_rate_returns_none(mock_get):
     fake_response = MagicMock()
     fake_response.json.return_value = {
         "rates": {
-            # CLP is intentionally missing
+            # CLP intentionally missing
             "EUR": 0.92
         }
     }
@@ -37,7 +37,7 @@ def test_fetch_usd_to_clp_missing_rate_returns_none(mock_get):
 
     assert result is None
 
-@patch("etl_modules.exchange_rate.requests.get")
+@patch("etl_modules.usd_to_clp.requests.get")
 def test_fetch_usd_to_clp_raises_exception_returns_none(mock_get):
     mock_get.side_effect = Exception("API down")
 
