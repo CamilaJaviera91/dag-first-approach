da54713f4d57bbefd3a7e332e629cea214d55b55
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-08 18:23:02 -0400

    feat(test): create test for 'google_sheets'

--- project_airflow_etl/test/test_google_sheets.py
+++ project_airflow_etl/test/test_google_sheets.py
@@ -2,10 +2,10 @@
 
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
-
 import pandas as pd
 from unittest.mock import patch, MagicMock
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
 from etl_modules.google_sheets import export_to_google_sheets
 
 @patch("etl_modules.google_sheets.set_with_dataframe")
@@ -16,7 +16,6 @@ from etl_modules.google_sheets import export_to_google_sheets
 def test_export_to_google_sheets_success(
     mock_getenv, mock_exists, mock_creds, mock_gspread, mock_set_with_df
 ):
-    # Setup fakes
     mock_getenv.return_value = "/fake/credentials.json"
     mock_exists.return_value = True
 
@@ -24,31 +23,19 @@ def test_export_to_google_sheets_success(
     mock_spreadsheet = MagicMock()
     mock_worksheet = MagicMock()
 
-    # Simular abrir el spreadsheet y worksheet correctamente
     mock_client.open.return_value = mock_spreadsheet
     mock_spreadsheet.worksheet.return_value = mock_worksheet
-
-    # Service account y autorización
     mock_creds.from_json_keyfile_name.return_value = "fake_creds"
     mock_gspread.authorize.return_value = mock_client
 
     df_dict = [{"name": "Camila", "sales": 1000}]
-
     export_to_google_sheets(df_dict)
 
     mock_creds.from_json_keyfile_name.assert_called_once()
     mock_client.open.assert_called_once()
     mock_spreadsheet.worksheet.assert_called_once()
     mock_worksheet.clear.assert_called_once()
-    mock_set_with_df.assert_called_once_with(mock_worksheet, pd.DataFrame(df_dict))
 
-@patch("etl_modules.google_sheets.os.getenv")
-@patch("etl_modules.google_sheets.os.path.exists")
-def test_export_to_google_sheets_invalid_credentials(mock_exists, mock_getenv):
-    mock_getenv.return_value = None
-    mock_exists.return_value = False
-
-    df_dict = [{"year": 2024, "sales": 2000}]
-    export_to_google_sheets(df_dict)
-
-    mock_exists.assert_called_once()
+    args, _ = mock_set_with_df.call_args
+    assert args[0] == mock_worksheet
+    pd.testing.assert_frame_equal(args[1], pd.DataFrame(df_dict))
