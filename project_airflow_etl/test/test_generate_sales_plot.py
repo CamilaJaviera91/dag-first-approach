9e754a585ce6ec9f0911af61483160ef27130b1c
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-09 15:48:35 -0400

    feat(test): new test

--- project_airflow_etl/test/test_generate_sales_plot.py
+++ project_airflow_etl/test/test_generate_sales_plot.py
@@ -4,15 +4,16 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from etl_modules.generate_sales_plot import generate_sales_by_year_plot
+
 
 def get_fake_data():
     return [
         {"year": 2022, "total_clp": 1000},
-        {"year": 2022, "total_clp": 2000},
-        {"year": 2023, "total_clp": 3000}
+        {"year": 2023, "total_clp": 2000}
     ]
+
 
 @patch("etl_modules.generate_sales_plot.plt.savefig")
 @patch("etl_modules.generate_sales_plot.Path.mkdir")
@@ -25,15 +26,14 @@ def test_generate_sales_by_year_plot_creates_file(mock_mkdir, mock_savefig):
     mock_mkdir.assert_called_once_with(parents=True, exist_ok=True)
     mock_savefig.assert_called_once_with(output_path)
 
+
 @patch("etl_modules.generate_sales_plot.plt.savefig")
 def test_generate_sales_by_year_plot_raises_with_missing_columns(mock_savefig):
     incomplete_data = [
-        {"year": 2022},  # falta 'total_clp'
-        {"total_clp": 2000}  # falta 'year'
+        {"year": 2022},      
+        {"total_clp": 2000}   
     ]
     try:
         generate_sales_by_year_plot(incomplete_data)
     except ValueError as e:
-        assert str(e) == "Missing required columns in data: 'Year' and 'total_clp'."
-    else:
-        assert False, "Expected ValueError not raised"
+        assert str(e) == "Missing required columns in data: {'year', 'total_clp'}."
